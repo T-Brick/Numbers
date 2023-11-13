0959892835a6218ffd939e8110cda97ffe7b1e66
T-Brick <thea@theabrick.com>
2023-11-13 01:37:48 -0500

    add IntN abbrev's for signed numbers

--- Numbers.lean
+++ Numbers.lean
@@ -7,7 +7,13 @@ abbrev Unsigned8     := Unsigned ⟨8, by simp⟩
 abbrev Unsigned16    := Unsigned ⟨16, by simp⟩
 abbrev Unsigned32    := Unsigned ⟨32, by simp⟩
 abbrev Unsigned64    := Unsigned ⟨64, by simp⟩
+
 abbrev Signed8       := Signed ⟨8, by simp⟩
 abbrev Signed16      := Signed ⟨16, by simp⟩
 abbrev Signed32      := Signed ⟨32, by simp⟩
 abbrev Signed64      := Signed ⟨64, by simp⟩
+
+abbrev Int8 := Signed8
+abbrev Int16 := Signed16
+abbrev Int32 := Signed32
+abbrev Int64 := Signed64
