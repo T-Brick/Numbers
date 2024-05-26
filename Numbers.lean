ff9e6cc0591f95787eda233994a7314fbdd4f20b
T-Brick <thea@theabrick.com>
2024-05-26 11:57:06 -0400

    add notation import

--- Numbers.lean
+++ Numbers.lean
@@ -1,17 +1,18 @@
 import Numbers.IntRepr
 import Numbers.IntOps
+import Numbers.Notation
 
 open Numbers
 
-abbrev Unsigned8     := Unsigned ⟨8 , by simp⟩
-abbrev Unsigned16    := Unsigned ⟨16, by simp⟩
-abbrev Unsigned32    := Unsigned ⟨32, by simp⟩
-abbrev Unsigned64    := Unsigned ⟨64, by simp⟩
+abbrev Unsigned8     := Unsigned ⟨8 , by decide⟩
+abbrev Unsigned16    := Unsigned ⟨16, by decide⟩
+abbrev Unsigned32    := Unsigned ⟨32, by decide⟩
+abbrev Unsigned64    := Unsigned ⟨64, by decide⟩
 
-abbrev Signed8       := Signed ⟨8 , by simp⟩
-abbrev Signed16      := Signed ⟨16, by simp⟩
-abbrev Signed32      := Signed ⟨32, by simp⟩
-abbrev Signed64      := Signed ⟨64, by simp⟩
+abbrev Signed8       := Signed ⟨8 , by decide⟩
+abbrev Signed16      := Signed ⟨16, by decide⟩
+abbrev Signed32      := Signed ⟨32, by decide⟩
+abbrev Signed64      := Signed ⟨64, by decide⟩
 
 abbrev Int8  := Signed8
 abbrev Int16 := Signed16
